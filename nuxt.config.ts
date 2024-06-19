493934a927f2877becad73547cb7d161d3e72a08
aiokr <hiaiokr@gmail.com>
2024-06-19 17:05:26 +0800

    feat: 优化动画样式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/content", "@pinia/nuxt", "@nuxtjs/color-mode", "@nuxtjs/turnstile"],
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/content", "@pinia/nuxt", "@nuxtjs/color-mode", "@nuxtjs/turnstile", "@vueuse/motion/nuxt"],
   srcDir: 'src/',
   imports: {
     dirs: ['./stores']
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,7 +3,15 @@
     <section class="h-[calc(80vh-64px)] w-full mt-[10vh] flex justify-center items-center">
       <div class="">
         <div class="text-4xl font-bold p-2">你好，我是</div>
-        <div class="text-8xl font-bold marked p-2">aiokr</div>
+        <div class="inline-block text-8xl font-bold marked p-2">aiokr</div>
+        <div v-motion id="linksDock" class="border rounded-lg w-max mt-8 px-3 h-11 flex justify-start items-end"
+          @mouseenter="onMouseenter" @mousemove="onMousemove" @mouseleave="onMouseleave">
+          <NuxtLink v-for="link in links" :key="link.link" :to="link.link" target="_blank"
+            :style="'width:' + link.width + 'px; height:' + iconWidth.value + 'px;'"
+            class="linksDockItem flex items-center justify-center aspect-square rounded-full hover:bg-dtext/40 transition-colors">
+            <component :is="link.icon" />
+          </NuxtLink>
+        </div>
       </div>
     </section>
     <article class="container max-w-[800px] mx-auto">
@@ -16,7 +24,11 @@
   </main>
 </template>
 
-<script setup lang="ts">
+<script setup>
+import { useMotions } from '@vueuse/motion'
+import IconsTwitter from '~/components/icons/Twitter.vue';
+import IconYoutube from '~/components/icons/Youtube.vue';
+
 useHead({
   title: '关于 - Tripper Press',
   meta: [
@@ -30,6 +42,105 @@ useSeoMeta({
   description: 'Take Photo, Think Seriousl',
   ogDescription: 'Take Photo, Think Seriousl'
 })
+
+// 默认图标宽度
+let iconWidth = ref(42);
+
+const links = ref([
+  {
+    title: 'X',
+    width: iconWidth.value,
+    link: 'https://x.com/skomobi',
+    icon: IconsTwitter
+  },
+  {
+    title: 'Youtube',
+    width: iconWidth.value,
+    link: 'https://youtube.com/@tripress',
+    icon: IconYoutube
+  }
+])
+
+let iconOriWidth = 42;
+let iconBigWidth = 64;
+
+
+// Dock 元素
+import { ref, onMounted } from 'vue';
+
+const linksDock = ref(null);
+const linksDockItem = ref([]);
+
+onMounted(() => {
+  linksDock.value = document.getElementById('linksDock');
+  linksDockItem.value = document.querySelectorAll('.linksDockItem');
+});
+
+const isMouseIntoDock = ref(false)
+
+const onMouseenter = () => {
+  isMouseIntoDock.value = true;
+}
+
+// 鼠标移动事件
+const onMousemove = (e) => {
+  // 获取鼠标位置
+  const x = e.clientX;
+  const y = e.clientY;
+  const dockRect = linksDock.value?.getBoundingClientRect(); // 获取 Dock 元素的位置
+  const isMouseInDock = x > dockRect.x && x < dockRect.x + dockRect.width && y > dockRect.y && y < dockRect.y + dockRect.height; // 鼠标是否在 Dock 元素内
+
+  // 每个图标大小不同
+  linksDockItem.value.forEach((item, index) => {
+    const itemRect = item.getBoundingClientRect(); // 获取图标元素的位置
+    const isMouseInItem = x > itemRect.x && x < itemRect.x + itemRect.width && y > itemRect.y && y < itemRect.y + itemRect.height; // 鼠标是否在图标元素内
+    const distance = Math.sqrt(Math.pow(x - itemRect.x - itemRect.width / 2, 2) + Math.pow(y - itemRect.y - itemRect.height / 2, 2)); // 计算鼠标与图标的距离
+    const iconNewWidth = iconOriWidth + (iconBigWidth - iconOriWidth) * (1 - distance / 240); // 计算图标新的大小
+
+    if (isMouseInDock && distance < 240) {
+      item.style.width = iconNewWidth + 'px';
+      item.style.height = iconNewWidth + 'px';
+    }
+  });
+}
+
+// 鼠标离开事件
+const onMouseleave = () => {
+  linksDockItem.value.forEach((item, index) => {
+    // 当前
+    const currentWidth = parseInt(item.style.width);
+    const currentHeight = parseInt(item.style.height);
+    // 目标
+    const targetWidth = iconOriWidth;
+    const targetHeight = iconOriWidth;
+    // 持续时间
+    const duration = 160;
+
+    const startTime = performance.now();// 动画开始时间
+
+    // 动画函数
+    const animate = (timestamp) => {
+      const elapsed = timestamp - startTime;
+      const progress = Math.min(elapsed / duration, 1); // Calculate the progress of the animation
+
+      const newWidth = currentWidth + (targetWidth - currentWidth) * progress;
+      const newHeight = currentHeight + (targetHeight - currentHeight) * progress;
+
+      item.style.width = newWidth + 'px';
+      item.style.height = newHeight + 'px';
+
+      if (progress < 1) {
+        requestAnimationFrame(animate); // Continue the animation
+      }
+    };
+
+    requestAnimationFrame(animate);
+  });
+  isMouseIntoDock.value = true;
+}
+
+
+
 
 </script>
 
