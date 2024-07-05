ad158e23b1ad4bedc400a8d6ba318f07cf7bad14
aiokr <hiaiokr@gmail.com>
2024-07-05 14:24:57 +0800

    build(nuxt): update nuxt to 3.12.3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/tailwindcss',
     "@nuxt/content",
@@ -10,12 +11,16 @@ export default defineNuxtConfig({
     "@vueuse/motion/nuxt",
     "@nuxtjs/seo",
     "@nuxtjs/sitemap",
-    "@nuxthq/studio"
+    "@nuxthq/studio",
+    "@nuxt/image"
   ],
+
   srcDir: 'src/',
+
   imports: {
     dirs: ['./stores']
   },
+
   components: [
     {
       path: '~/components/',
@@ -26,23 +31,28 @@ export default defineNuxtConfig({
       pathPrefix: true,
     }
   ],
+
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     }
   },
+
   site: {
     url: 'https://tripper.press',
     name: 'Tripper Press - Take Photo, Think Seriously.',
     description: 'Take Photo, Think Seriously.',
   },
+
   ogImage: { enabled: false },
+
   sitemap: {
     sources: [
       '/api/postmap'
     ]
   },
+
   content: {
     highlight: {
       theme: {
@@ -63,22 +73,28 @@ export default defineNuxtConfig({
       }
     }
   },
+
   colorMode: {
     preference: "system",
     fallback: "light",
     classSuffix: '',
   },
+
   turnstile: {
     siteKey: '0x4AAAAAAAcv6femMitqO6lO',
   },
+
   nitro: {
     prerender: {
       routes: ["/atom.xml", "/sitemap.xml", "/api/postmap"],
     },
   },
+
   runtimeConfig: {
     turnstile: {
       secretKey: '',
     },
-  }
+  },
+
+  compatibilityDate: '2024-07-05'
 })
