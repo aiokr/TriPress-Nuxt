e199d5f9711d8458db017e2475295b2ac898146b
aiokr <hiaiokr@gmail.com>
2024-07-07 23:09:24 +0800

    feat: add config type and uses page

---
+++ src/index.d.ts
@@ -0,0 +1,25 @@
+declare module 'nuxt/schema' {
+  interface AppConfigInput {
+    content: {
+      travel: { destination: string, already?: boolean }[]
+      reading: {
+        bookName: string
+        link: string
+        cover: string
+      }
+      uses: {
+        camera: { model: string, status: 'using' | 'used' | 'sold' | 'broken' }[]
+        lens: { model: string, status: 'using' | 'sold' | 'broken' }[]
+        film: { model: string, status: 'using' | 'sold' | 'broken', image?: string }[]
+        laptop: { model: string, status: 'using' | 'sold' | 'broken' }[]
+        PC: {
+          mouse: { model: string }
+          keyboard: { model: string }
+        }[]
+      }
+    }
+  }
+}
+
+// It is always important to ensure you import/export something when augmenting a type
+export { }
