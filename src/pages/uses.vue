db0fb150293e4e692c429a4d718d45cb19425720
aiokr <hiaiokr@gmail.com>
2024-07-08 12:58:29 +0800

    feat: add camera and film

--- src/pages/uses.vue
+++ src/pages/uses.vue
@@ -8,13 +8,30 @@
       <hr />
     </div>
     <div class="pt-4">
+      <div class="text-2xl font-bold text-text dark:text-dtext pb-4">Camera</div>
+      <div class="grid grid-cols-2 gap-5 pb-8">
+        <div v-for="i in usesData.camera" :key="i.model">
+          <NuxtImg v-if="i.image" :src="i.image" :alt="i.model" class="rounded-lg border" />
+          <div class="flex gap-2 items-center justify-between uppercase pt-1">
+            <div class="text-sm py-2 inline-block">{{ i.model }}</div>
+            <div v-if="i.status === 'plan' || 'using' || 'used'"
+              class="py-1 px-2 text-xs border rounded-full inline-block">
+              {{ i.status }}
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="text-2xl font-bold text-text dark:text-dtext pb-4">Film</div>
-      <div class="grid grid-cols-2 md:grid-cols-5 grid-4">
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-5">
         <div v-for="i in usesData.film" :key="i.model">
           <NuxtImg v-if="i.image" :src="i.image" :alt="i.model" class="rounded-lg border" />
-          <NuxtLink v-if="i.link" :to="i.link" class="text-lg font-bold text-text dark:text-dtext">{{ i.model }}
-          </NuxtLink>
-          <div class="text-sm py-2">{{ i.model }}</div>
+          <div class="flex gap-2 items-center justify-between uppercase pt-1">
+            <div class="text-sm py-2 inline-block">{{ i.model }}</div>
+            <div v-if="i.status === 'plan' || 'using' || 'used'"
+              class="py-1 px-2 text-xs border rounded-full inline-block">
+              {{ i.status }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
