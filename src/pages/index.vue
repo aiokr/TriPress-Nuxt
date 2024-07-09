b60841b853eb4f0c63e10ea2598c42e1d234aea9
aiokr <hiaiokr@gmail.com>
2024-07-09 13:03:38 +0800

    feat(index): 首页「建造中」Banner 添加底纹

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="container max-w-[1000px] px-2 md:px-0 mx-auto">
     <section
-      class="w-full transition-shadow rounded px-4 md:px-3 pt-16 md:pt-32 pb-20 md:pb-36 flex flex-col md:flex-row md:items-center justify-between gap-12">
+      class="w-full transition-shadow rounded px-4 md:px-3 pt-16 md:pt-32 pb-20 md:pb-36 flex flex-col md:flex-row md:items-center justify-between gap-12"
+    >
       <div class="relative md:hidden">
-        <img src="https://imgur.lzmun.com/picgo/after2022/tripper2colorfull.png_avatar" width="64px" height="64px">
-        <div class="absolute inset-0 -z-10 bg-gradient-to-tl from-main/60 to-main/10 opacity-0 blur-2xl md:opacity-50">
-        </div>
+        <img src="https://imgur.lzmun.com/picgo/after2022/tripper2colorfull.png_avatar" width="64px" height="64px" />
+        <div class="absolute inset-0 -z-10 bg-gradient-to-tl from-main/60 to-main/10 opacity-0 blur-2xl md:opacity-50"></div>
       </div>
       <div class="text-3xl font-bold">
-        <div class="pb-2">I'm aiokr,<br />
+        <div class="pb-2">
+          I'm aiokr,<br />
           want to be a
           <div class="wantToBeGrid inline-grid h-10 text-main overflow-hidden">
             <div class="grid gap-1" :style="wantToBeStyle">
@@ -22,16 +23,15 @@
         </div>
       </div>
       <div class="hidden md:block md:relative">
-        <img src="https://imgur.lzmun.com/picgo/after2022/tripper2colorfull.png_avatar" width="72px" height="72px">
-        <div class="absolute inset-0 -z-10 bg-gradient-to-tl from-main/60 to-main/10 opacity-0 blur-2xl md:opacity-50">
-        </div>
+        <img src="https://imgur.lzmun.com/picgo/after2022/tripper2colorfull.png_avatar" width="72px" height="72px" />
+        <div class="absolute inset-0 -z-10 bg-gradient-to-tl from-main/60 to-main/10 opacity-0 blur-2xl md:opacity-50"></div>
       </div>
     </section>
     <div class="flex items-center justify-between px-3 md:px-2">
       <div class="text-3xl font-bold pb-6">About Me</div>
     </div>
     <section class="grid grid-cols-1 md:grid-cols-2 gap-6 px-3 md:px-2 pb-12">
-      <div class="flex-1 md:col-span-2 shadow-card dark:shadow-card-dark rounded-lg p-3">
+      <div class="warnBanner flex-1 md:col-span-2 shadow-card dark:shadow-card-dark rounded-lg p-3">
         <div class="font-bold py-2 text-center">Building……</div>
       </div>
       <div class="flex-1 shadow-card dark:shadow-card-dark rounded-lg p-3">
@@ -52,8 +52,7 @@
         <div class="text-xs text-zinc-300 dark:text-dtext/40 pt-2 pb-1">stay hungry, stay foolish</div>
         <div class="text-lg font-bold">Currently Reading</div>
         <a :href="currentReadingBookLink" target="_blank">
-          <img :src="currentReadingBookCover"
-            class="bookCover h-40 w-32 object-cover overflow-hidden mx-auto translate-y-3" />
+          <img :src="currentReadingBookCover" class="bookCover h-40 w-32 object-cover overflow-hidden mx-auto translate-y-3" />
           <div class="text-center text-2xl font-bold">{{ currentReadingBook }}</div>
         </a>
       </div>
@@ -63,12 +62,18 @@
       <NuxtLink to="/posts" class="text-sm text-main">All Posts</NuxtLink>
     </div>
     <section class="container mx-auto px-3 md:px-2 pb-16 grid grid-cols-1 md:grid-cols-2 gap-6">
-      <NuxtLink v-for="post in posts" :to="post._path"
-        class="postCard block border-zinc-400/20 dark:border-white/20 w-full shadow-card dark:shadow-card-dark hover:shadow-lg rounded-xl bg-white dark:bg-zinc-800">
-        <img v-if="post.cover" :src="post.cover" :alt="post.title + ' cover'"
-          class="h-[180px] md:h-[233px] w-full object-cover rounded-2xl p-2" />
-        <img v-else src="https://imgur.lzmun.com/tricms/1713761252690-2022-03-16-DSC09021-web.jpg"
-          :alt="post.title + ' default cover'" class="h-[180px] md:h-[233px] w-full object-cover rounded-2xl p-2" />
+      <NuxtLink
+        v-for="post in posts"
+        :to="post._path"
+        class="postCard block border-zinc-400/20 dark:border-white/20 w-full shadow-card dark:shadow-card-dark hover:shadow-lg rounded-xl bg-white dark:bg-zinc-800"
+      >
+        <img v-if="post.cover" :src="post.cover" :alt="post.title + ' cover'" class="h-[180px] md:h-[233px] w-full object-cover rounded-2xl p-2" />
+        <img
+          v-else
+          src="https://imgur.lzmun.com/tricms/1713761252690-2022-03-16-DSC09021-web.jpg"
+          :alt="post.title + ' default cover'"
+          class="h-[180px] md:h-[233px] w-full object-cover rounded-2xl p-2"
+        />
         <div class="h-[200px] p-3 md:p-4 md:pt-4 flex flex-col justify-between">
           <div>
             <div v-if="post.date" class="text-sm text-zinc-400 dark:text-dtext/80 pb-2">
@@ -154,7 +159,6 @@ onMounted(() => {
 });
 
 const posts = await queryContent("/post").only(["_path", "title", "date", "category", "cover", "type"]).where({ type: { $ne: 'draft' } }).limit(4).sort({ date: -1 }).find();
-
 </script>
 
 <style scoped>
@@ -177,5 +181,21 @@ const posts = await queryContent("/post").only(["_path", "title", "date", "categ
 
 .bookCover {
   mask-image: linear-gradient(black 0%, rgb(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.2) 80%, transparent 100%);
+}
+
+.warnBanner {
+  --orange: #ffc75f30;
+  --white: #ffc75f10;
+  background:
+  linear-gradient(90deg, #ffffff ,#ffffffb0 80%, #ffffff00 100%),
+  repeating-linear-gradient(-45deg,  var(--orange) 0%, var(--orange) 2%, var(--white) 2%, var(--white) 4%, var(--orange) 4%);
+}
+
+.dark .warnBanner{
+  --orange: #ffc75f60;
+  --white: #ffc75f40;
+  background:
+  linear-gradient(90deg, #18181b ,#18181bb0 80%, #18181b00 100%),
+  repeating-linear-gradient(-45deg,  var(--orange) 0%, var(--orange) 2%, var(--white) 2%, var(--white) 4%, var(--orange) 4%);
 }
 </style>
