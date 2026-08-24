c432b1ac73762ec9ad80b8ae20deca9d36038ed4
aiokr <hiaiokr@gmail.com>
2026-08-24 15:02:44 +0800

    style(article): 更新文章样式

--- src/components/content/Collapse.vue
+++ src/components/content/Collapse.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="block text-left border rounded-xl">
-    <button type="button" class="w-full collapseHeader text-left bg-[#71afdd] text-white rounded-t-lg px-3 py-2"
-      @click="toggle">
+  <div class="block text-left border rounded-xl mb-5">
+    <button v-if="$slots.collapseHeader" type="button"
+      class="w-full collapseHeader text-left bg-[#71afdd] text-white rounded-t-lg px-3 py-2" @click="toggle">
       <slot name="collapseHeader" />
     </button>
+    <button v-else type="button" class="w-full collapseHeader text-left bg-[#71afdd] text-white rounded-t-lg px-3 py-2"
+      @click="toggle">
+      Collapse
+    </button>
     <div class="collapseBody transition-[300]" style="overflow: hidden;"
+      :class="{ 'cursor-pointer': !$slots.collapseHeader }"
       :style="isOpen ? 'max-height: 400px; mask-image: none;'
-        : 'max-height: 80px; mask-image: linear-gradient(black 0%, rgb(0, 0, 0) 30%, rgba(0, 0, 0, 0.6) 80%, transparent 100%);'">
+        : 'max-height: 0px; mask-image: linear-gradient(black 0%, rgb(0, 0, 0) 30%, rgba(0, 0, 0, 0.6) 80%, transparent 100%);'">
       <slot />
     </div>
   </div>
@@ -24,14 +29,13 @@ const toggle = () => {
 
 <style>
 .collapseHeader p {
-  font-weight: 700;
-  font-size: 1.25rem;
-  line-height: 1.25rem;
+  font-size: 1rem;
+  line-height: 1rem;
   margin: 8px !important;
 }
 
 .collapseBody {
-  padding-top: 0.75rem;
+  padding-top: 0.4rem;
   margin: 8px 16px !important;
 }
 </style>
